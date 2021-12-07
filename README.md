3915e2498a7e1c74409f4ed891d9e498d4282a1d
Joel <broganjr@ornl.gov>
2021-12-06 22:50:34 -0500

    added more to readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-simple bio-inspired features
+This repository provides an incredibly light-weight implementation of the gray-scale feature extractor from:
+"Beyond simple features: A large-scale feature search approach to unconstrained face recognition"
+
+## Use
+
+1. `pip install simplefeat` or 
+   `git clone https://github.com/joelb92/simplefeature.git && cd simplefeat && python setup.py install`
+2. ~~~
+   git clone https://code.ornl.gov/ii1/briar-api.git`
+   import simplefeature
+   import cv2
+   im = cv2.imread("/home/face.jpg")
+   embedding = simplefeature.extract(im) 
+   ~~~
+
+Inputs will be scaled to 200x200px
+The system outputs a 51200-d vector
+
+Please cite this paper:
+~~~
+Cox, David, and Nicolas Pinto. "Beyond simple features: A large-scale feature search approach to unconstrained face recognition."
+2011 IEEE International Conference on Automatic Face & Gesture Recognition (FG). IEEE, 2011.
+~~~
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -8,8 +8,28 @@ setup(name='simplefeature',
       description='',
       long_description=long_description,
       author='D.D. Cox, and N. Pinto, Joel Brogan',
+      author_email="joelbrogan92@gmail.com",
       url='https://github.com/joelb92/simplefeature',
-      packages=['simplefeature'],
-      package_dir={'simplefeature': './'}
+      # packages=['simplefeature'],
+      package_dir={'simplefeature': './'},
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+     ],
+      install_requires=[
+          'opencv-python',
+          'numpy',
+          'scikit-image',
+      ],
      )
 
